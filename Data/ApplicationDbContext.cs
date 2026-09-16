40ddee3142f6a71a64274d9640275f1abccb36ba
John Alec Resos <johnalecresos@gmail.com>
2026-09-16 09:27:42 +0800

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using ECommerceInventory.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceInventory.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; } = null!;
+
+    public DbSet<Order> Orders { get; set; } = null!;
+
+    public DbSet<OrderItem> OrderItems { get; set; } = null!;
+}
